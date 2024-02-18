3b5b31588f2434417533bfcde249776041236406
Alexander Schmitt <119889394+AlexanderS-2261875@users.noreply.github.com>
2024-02-18 13:20:57 +0100

    about animation impl; optimized css animations

--- js/navBarScrolling.js
+++ js/navBarScrolling.js
@@ -6,17 +6,17 @@ window.onscroll = function() {
     let scrollY = window.scrollY;
     let navbar = document.getElementById('navbar');
     let aboutY = document.getElementById('about').offsetTop;
-    if (aboutY < scrollY) {
+    let pos = scrollY - aboutY
+    if (10 < scrollY) {
         navbar.style.opacity = '0.80';
         navbar.style.height = '50px';
     } else {
         navbar.style.opacity = '1';
         navbar.style.height = '60px';
     }
-    if (aboutY > 0){
+    if (pos > 0){
         navbar.style.background = 'rgba(255,255,255,0.15)';
     } else {
         navbar.style.background = 'rgba(0,0,0)';
     }
-    window.console.log(scrollY, aboutY)
 }
