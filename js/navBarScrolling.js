50b5385f4c486d3c6faf5970452fc08f889a6316
AlexanderS-2261875 <alexander.schmitt@student.uhasselt.be>
2024-03-28 16:04:25 +0100

    dark-light mode implementation animation removal, update at later time

--- js/navBarScrolling.js
+++ js/navBarScrolling.js
@@ -1,22 +1,32 @@
 /**
  * @author Alexander Schmitt
  */
+function handleScroll() {
+    const scrollY = window.scrollY;
+    const navbar = document.getElementById('navbar');
+    const aboutY = document.getElementById('about').offsetTop;
+    const pos = scrollY - aboutY;
 
-window.onscroll = function() {
-    let scrollY = window.scrollY;
-    let navbar = document.getElementById('navbar');
-    let aboutY = document.getElementById('about').offsetTop;
-    let pos = scrollY - aboutY
-    if (10 < scrollY) {
-        navbar.style.opacity = '0.80';
-        navbar.style.height = '50px';
-    } else {
-        navbar.style.opacity = '1';
-        navbar.style.height = '60px';
-    }
-    if (pos > 0){
-        navbar.style.background = 'rgba(255,255,255,0.15)';
-    } else {
-        navbar.style.background = 'rgba(0,0,0)';
-    }
+    navbar.style.opacity = scrollY > 10 ? '0.80' : '1';
+    navbar.style.height = scrollY > 10 ? '50px' : '60px';
+    navbar.style.background = !document.querySelector('.checkbox').checked && pos > 0 ? 'rgba(255,255,255,0.15)' : 'rgba(0,0,0)';
 }
+
+function handleCheckboxChange() {
+    const checkbox = document.querySelector('.checkbox');
+    const root = document.documentElement;
+    const colorMode = checkbox.checked ? '#f1f1f1' : '#000000';
+
+    root.style.setProperty('--colorMode', colorMode);
+    root.style.setProperty('--inverseColorMode', checkbox.checked ? '#000000' : '#ffffdf');
+    root.style.setProperty('--colorCardsOuter', checkbox.checked ? '26262659' : 'f1f1f1');
+    root.style.setProperty('--colorCardsInner', checkbox.checked ? '000000' : 'ffffff');
+    console.log(checkbox.state);
+}
+
+window.addEventListener('scroll', handleScroll);
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Add change event listener to checkbox
+    document.querySelector('.checkbox').addEventListener('change', handleCheckboxChange);
+});
