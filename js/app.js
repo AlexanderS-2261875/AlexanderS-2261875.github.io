80891f8d595ecfa969bfff96b3e6357a021d6288
AlexanderS-2261875 <alexander.schmitt@student.uhasselt.be>
2024-04-29 20:34:06 +0200

    nav change, freelance section added.

--- js/app.js
+++ js/app.js
@@ -6,30 +6,11 @@ function handleScroll() {
     const navbar = document.getElementById('navbar');
     const aboutY = document.getElementById('about').offsetTop;
     const pos = scrollY - aboutY;
-    navbar.style.opacity = scrollY > 10 ? '0.80' : '1';
-    navbar.style.height = scrollY > 10 ? '50px' : '60px';
-    navbar.style.background = !document.querySelector('.checkbox').checked && pos > 0 ? 'rgba(255,255,255,0.15)' : 'rgba(0,0,0)';
-}
-
-function handleCheckboxChange() {
-    const checkbox = document.querySelector('.checkbox');
-    const root = document.documentElement;
-    const colorMode = checkbox.checked ? '#f1f1f1' : '#000000';
-
-    root.style.setProperty('--colorMode', colorMode);
-    root.style.setProperty('--inverseColorMode', checkbox.checked ? '#000000' : '#f1f1f1');
-    root.style.setProperty('--colorCardsOuter', checkbox.checked ? '#f1f1f159' : '#26262659');
-    root.style.setProperty('--colorCardsInner', checkbox.checked ? '#ffffff' : '#000000');
-    console.log(checkbox.state);
+    navbar.style.background = pos > 0 ? 'var(--navDark)' : 'var(--navLight)';
+    navbar.classList.toggle('nav-dark', pos > 0);
 }
 
 window.addEventListener('scroll', handleScroll);
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Add change event listener to checkbox
-    document.querySelector('.checkbox').addEventListener('change', handleCheckboxChange);
-});
-
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open')
 }
